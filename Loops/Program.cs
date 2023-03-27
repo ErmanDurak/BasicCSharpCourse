b9cd5c2232f593cea4c8cb4c7172a7d281595a8a
ermandurak <ermandurak67@gmail.com>
2023-03-27 23:27:37 +0300

    Loops file filled

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -10,7 +10,53 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 10; i=i+1) // i++ = i=i+1  
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("---------");
 
+            for (int i = 1; i <= 10; i+=2) // i+=2 = i=i+2   
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("---------");
+
+            // Nested Loops
+            for (int i = 1; i <=2 ; ++i) 
+            {
+                Console.WriteLine("Outer: " + i);
+
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.WriteLine("Inner: " + j);
+                }
+            }
+            Console.WriteLine("---------");
+
+            int number1 = 11;
+            while (number1 >=0)
+            {
+                Console.WriteLine(number1);
+                number1--; // i-- = i - 1
+            }
+            Console.WriteLine("---------");
+
+            int number2 = 11;
+            do
+            {
+                Console.WriteLine(number2);
+                number2-=2;
+
+            } while (number2>=0);
+            Console.WriteLine("---------");
+
+            string[] countries= {"Turkey","Italy","Spain","England"};
+            foreach (var country in countries)
+            {
+                Console.WriteLine(country);
+            }
+            Console.ReadLine();
         }
     }
 }
